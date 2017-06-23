c365d90de6d5c2708c75c25cb1c05cf1151ea9d0
Mikhail Popov <mpopov@wikimedia.org>
2017-06-23 12:10:56 -0700

    Add syntax checking as test
    Change-Id: I12c18cd5376e3d4127d9dfb1e70f2d1559c93f25

--- tests/testthat/test-dwelltime.R
+++ tests/testthat/test-dwelltime.R
@@ -1,14 +1,26 @@
 context("dwelltime")
 
-sample_events <- read.csv(system.file("extdata", "sample_events.csv", package="ortiz"), stringsAsFactors = FALSE)
+sample_events <- read.csv(system.file("extdata", "sample_events.csv", package = "ortiz"), stringsAsFactors = FALSE)
 sample_events$timestamp <- as.POSIXct(sample_events$timestamp, format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")
 
 test_that("intertimes are correctly estimated", {
-  expect_equal(ortiz:::dwell_time_(split(x = sample_events[, "timestamp"], f = sample_events[, "session_id"])), c(31, 30))
+  expect_equal(
+    ortiz:::dwell_time_(split(
+      x = sample_events[, "timestamp"],
+      f = sample_events[, "session_id"]
+    )),
+    c(31, 30)
+  )
 })
 
 test_that("session intertimes are correctly estimated", {
-  expect_equal(ortiz:::dwell_time_(split(x = sample_events[c(1, 8:9, 18), "timestamp"], f = sample_events[c(1, 8:9, 18), "session_id"])), c(92, 150))
+  expect_equal(
+    ortiz:::dwell_time_(split(
+      x = sample_events[c(1, 8:9, 18), "timestamp"],
+      f = sample_events[c(1, 8:9, 18), "session_id"]
+    )),
+    c(92, 150)
+  )
 })
 
 test_that("threshold of 0 returns all TRUE", {
